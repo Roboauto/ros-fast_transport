826fab99a70e15ead5193846cc41562215ad2ce2
Tomáš Černík <tomas.cernik@artin.cz>
2017-07-24 15:03:01 +0200

    refactored

--- include/fast_transport.h
+++ include/fast_transport.h
@@ -1,39 +1,50 @@
 #pragma once
 
-#include <string>
+#include <std_msgs/Int64.h>
+#include <std_msgs/Header.h>
 
 #include <ros/ros.h>
 
-#include <fast_transport_msgs/FastMsg.h>
+#include <string>
 
 template<class T>
 class fast_transport {
 public:
 
+    static class unique_id {
+        public:
+        int GetId() {
+            //TODO: implement
+            return 1;
+        }
+    } unique_id;
+
     ros::Subscriber fast_subscriber;
 
     struct Publisher {
         ros::Publisher pub;
+        ros::Publisher pub_info;
         ros::Publisher fast_pub;
 
         template<class D>
         T GetMsg(const D& data, const std_msgs::Header &header);
 
         template<class D>
-        fast_transport_msgs::FastMsg GetFastMsg(const D& data, const std_msgs::Header &header);
-
-        template<class D>
         void publish(const D& data, const std_msgs::Header &header);
     };
 
-    static ros::Subscriber subscribe<T>() {
+    static ros::Subscriber subscribe() {
         return {};
     }
 
     static Publisher advertise(ros::NodeHandle&n, const std::string & topic) {
         Publisher pub;
         pub.pub = n.advertise<T>(topic, 1);
-        pub.fast_pub = n.advertise<T>("/fast_transport/"+topic, 1);
+        pub.fast_pub = n.advertise<std_msgs::Header>("/fast_transport/"+topic, 1);
+        pub.pub_info = n.advertise<std_msgs::Int64>("/fast_transport/info/"+topic, 1,true);
+        std_msgs::Int64 msg;
+        msg.data = unique_id.GetId();
+        pub.pub_info.publish(msg);
         return pub;
     }
 
@@ -50,9 +61,6 @@ void fast_transport<T>::Publisher::publish(const D &data, const std_msgs::Header
         pub.publish(GetMsg(data,header));
     }
     if(fast_pub.getNumSubscribers() != 0) {
-        fast_transport_msgs::FastMsg msg;
-        msg.header = header;
-        msg.data = "TEST";
-        fast_pub.publish(GetFastMsg(data,header));
+        fast_pub.publish(header);
     }
 }
